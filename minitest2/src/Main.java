35cdc6355648b8d1c363abd5ffc0e161bbb46463
icedck <charm30072002@gmail.com>
2025-04-11 16:30:34 +0700

    minitest23

--- minitest2/src/Main.java
+++ minitest2/src/Main.java
@@ -1,31 +1,61 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Product product[] = { new Product(001, "cake", 5000.0, 30),
+        Product product[] = {new Product(001, "cake", 5000.0, 30),
                 new Product(002, "candy", 3000000.0, 1),
                 new Product(003, "snack", 5000000.0, 6),
                 new Product(004, "milk", 6000.0, 50),
                 new Product(005, "water", 8000.0, 20),
         };
-        System.out.println("Danh sách sản phẩm: ");
-        extracted(product);
 
-        System.out.println("Tìm kiếm sản phẩm theo tên chính xác: ");
-        findName(product, "cake");
-
-        System.out.println("Danh sách các sản phẩm có giá > 1000000: ");
-        displayProduct(product);
-
-        System.out.println("sản phẩm có số lượng tồn kho lớn nhất: ");
-        LargestInventory(product);
-
-        System.out.println("tổng số sản phẩm: " + countProduct(product) + ", tổng giá trị tồn kho: " +
-                totalInventoryValue(product) + ", giá trung bình: " + Product.getAveragePrice(product));
-
-        System.out.println("Thay đổi tên kho: ");
-        Product.setStoreName("Kho miền Nam");
-        extracted(product);
+        int choice = -1;
+        Scanner sc = new Scanner(System.in);
+        while (choice != 0) {
+            System.out.println("Menu");
+            System.out.println("1. Danh sách sản phẩm");
+            System.out.println("2. Tìm kiếm sản phẩm theo tên chính xác");
+            System.out.println("3. Danh sách các sản phẩm có giá > 1000000");
+            System.out.println("4. Sản phẩm có số lượng tồn kho lớn nhất");
+            System.out.println("5. Tổng số sản phẩm, tổng giá trị tồn kho, giá trung bình");
+            System.out.println("6. Thay đổi tên kho");
+            System.out.println("0. Exit");
+            System.out.print("Enter your choice: ");
+            choice = sc.nextInt();
+            switch (choice) {
+                case 1:
+                    System.out.println("Danh sách sản phẩm: ");
+                    extracted(product);
+                    break;
+                case 2:
+                    System.out.println("Tìm kiếm sản phẩm theo tên chính xác: ");
+                    findName(product, "cake");
+                    break;
+                case 3:
+                    System.out.println("Danh sách các sản phẩm có giá > 1000000: ");
+                    displayProduct(product);
+                    break;
+                case 4:
+                    System.out.println("sản phẩm có số lượng tồn kho lớn nhất: ");
+                    LargestInventory(product);
+                    break;
+                case 5:
+                    System.out.println("tổng số sản phẩm: " + countProduct(product) + ", tổng giá trị tồn kho: " +
+                            totalInventoryValue(product) + ", giá trung bình: " + Product.getAveragePrice(product));
+                    break;
+                case 6:
+                    System.out.println("Thay đổi tên kho: ");
+                    Product.setStoreName("Kho miền Nam");
+                    extracted(product);
+                    break;
+                case 0:
+                    System.exit(0);
+                 default:
+                     System.out.println("Invalid choice");
+            }
+        }
     }
 
     public static void extracted(Product[] product) {
@@ -34,7 +64,7 @@ public class Main {
         }
     }
 
-    public static String findName(Product arr[], String ProductName){
+    public static String findName(Product arr[], String ProductName) {
         for (int i = 0; i < arr.length; i++) {
             if (arr[i].getName().equals(ProductName)) {
                 arr[i].display();
@@ -43,25 +73,25 @@ public class Main {
         return null;
     }
 
-    public static void displayProduct(Product arr[]){
+    public static void displayProduct(Product arr[]) {
         for (int i = 0; i < arr.length; i++) {
-            if (arr[i].getPrice() > 1000000){
+            if (arr[i].getPrice() > 1000000) {
                 arr[i].display();
             }
         }
     }
 
-    public static void LargestInventory(Product arr[]){
+    public static void LargestInventory(Product arr[]) {
         int max = arr[0].getQuantity();
         for (int i = 0; i < arr.length; i++) {
-            if (max < arr[i].getQuantity()){
+            if (max < arr[i].getQuantity()) {
                 max = arr[i].getQuantity();
                 arr[i].display();
             }
         }
     }
 
-    public static int countProduct(Product arr[]){
+    public static int countProduct(Product arr[]) {
         int count = 0;
         for (int i = 0; i < arr.length; i++) {
             count++;
@@ -69,7 +99,7 @@ public class Main {
         return count;
     }
 
-    public static double totalInventoryValue(Product arr[]){
+    public static double totalInventoryValue(Product arr[]) {
         double total = 0;
         for (int i = 0; i < arr.length; i++) {
             total += arr[i].getInventoryValue();
